282ecab8c913b6fc5344cffd2d7f161b0b090898
Tatiana Lazareva <tatiana.lazareva86@gmail.com>
2021-10-02 18:59:01 +0200

    cz 1

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,16 @@ public class Main {
     public static void main(String[] args) {
 //        ćw.1 napisać po kilka przykładów wypisywania tekstu w konsoli,
 //        Użyć każdej z opcji
-        System.out.println("tatiana");
+        System.out.println("Witaj Tatiano!");
+        System.out.println("To pierwsze zajecie z Java:)");
+        System.out.println("Zachwilę juz do domu");
+
+        System.out.print("Kocham swoich ");
+        System.out.print("koty i ");
+        System.out.print("pieska");
+        System.out.println();
+
+        System.out.printf( "A %s kochaja mnie. Mam %d koty i jednego psa.\n", "oni", 2);
 
     }
 }
